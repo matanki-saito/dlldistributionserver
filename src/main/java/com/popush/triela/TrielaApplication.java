8cd974ca3674606037a190b747ae3cc743ee5758
matanki-saito <matanki.saito@gmail.com>
2018-12-24 02:40:42 +0900

    initilizer

---
+++ src/main/java/com/popush/triela/TrielaApplication.java
@@ -0,0 +1,14 @@
+package com.popush.triela;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrielaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TrielaApplication.class, args);
+    }
+
+}
+
