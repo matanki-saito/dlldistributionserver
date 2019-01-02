0d7eb4ab881f040eec203c0f366f576bca3d6286
matanki-saito <matanki.saito@gmail.com>
2019-01-02 17:45:41 +0900

    #6

--- src/main/java/com/popush/triela/TrielaApplication.java
+++ src/main/java/com/popush/triela/TrielaApplication.java
@@ -2,13 +2,13 @@ package com.popush.triela;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class TrielaApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(TrielaApplication.class, args);
     }
-
 }
 
