fa2850521fe91d176184b7d87db206ead982d433
matanki.saito <matanki.saito@gmail.com>
2020-06-28 20:26:07 +0900

    キャッシュ設定

--- src/main/java/com/popush/triela/TrielaApplication.java
+++ src/main/java/com/popush/triela/TrielaApplication.java
@@ -2,12 +2,10 @@ package com.popush.triela;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
 @EnableAsync
-@EnableCaching
 public class TrielaApplication {
     public static void main(String[] args) {
         SpringApplication.run(TrielaApplication.class, args);
