3b585cb3f0e9eb5542d51fa585d3634f3315b298
matanki.saito <matanki.saito@gmail.com>
2020-08-23 21:11:24 +0900

    grpcのテストコード追加
     ktlinを削除したので、code 206 エラーがなくなった

---
+++ src/main/java/com/popush/triela/api/internal/InternalDemoService.java
@@ -0,0 +1,17 @@
+package com.popush.triela.api.internal;
+
+import io.grpc.examples.helloworld.GreeterGrpc;
+import io.grpc.examples.helloworld.HelloReply;
+import io.grpc.examples.helloworld.HelloRequest;
+import io.grpc.stub.StreamObserver;
+import org.lognet.springboot.grpc.GRpcService;
+
+@GRpcService
+public class InternalDemoService extends GreeterGrpc.GreeterImplBase {
+    @Override
+    public void sayHello(HelloRequest request, StreamObserver<HelloReply> responseObserver) {
+        HelloReply reply = HelloReply.newBuilder().setMessage("Hello " + request.getName()).build();
+        responseObserver.onNext(reply);
+        responseObserver.onCompleted();
+    }
+}
